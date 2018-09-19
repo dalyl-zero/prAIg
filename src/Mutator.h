c1f9dcc83f1c9ec95ee832cc24c83cfbbb27a850
lapinozz <monstrefou@gmail.com>
2018-09-18 21:40:55 -0400

    refactored privacy of mutator

--- src/Mutator.h
+++ src/Mutator.h
@@ -17,20 +17,18 @@ public:
         int weight;
     };
     using OpCodeWeights = std::vector<OpCodeWeight>;
-
-    int totalOpCodeWeights = 0;
-    OpCodeWeights opCodeWeights = {};
     void setOpCodeWeights(const OpCodeWeights& weights);
+
 
     enum CodeMutation
     {
-    	INCREMENT_OPERAND,
-    	DECREMENT_OPERAND,
-    	INCREMENT_REGISTER,
-    	DECREMENT_REGISTER,
-    	NEW_OPCODE,
-    	NEW_OPERAND,
-    	NEW_REGISTER
+        INCREMENT_OPERAND,
+        DECREMENT_OPERAND,
+        INCREMENT_REGISTER,
+        DECREMENT_REGISTER,
+        NEW_OPCODE,
+        NEW_OPERAND,
+        NEW_REGISTER
     };
 
     struct CodeMutationWeight
@@ -40,11 +38,26 @@ public:
     };
     using CodeMutationWeights = std::vector<CodeMutationWeight>;
 
-    int totalCodeMutationWeights = 0;
-    CodeMutationWeights codeMutationWeights = {};
     void setCodeMutationWeights(const CodeMutationWeights& weights);
 
     void saveProgramToFile(const Program& program, const std::string& path);
+
+    Program generateProgram(int programSize = 100);
+
+    void mutate(Program& program);
+
+    std::uniform_int_distribution<int> randomOperand;
+    std::uniform_int_distribution<int> randomRegister;
+    std::uniform_int_distribution<int> randomMutate;
+
+private:    
+
+    int totalOpCodeWeights = 0;
+    OpCodeWeights opCodeWeights = {};
+
+    int totalCodeMutationWeights = 0;
+    CodeMutationWeights codeMutationWeights = {};
+
     void insert(const Code& code, Program& program);
     void remove(Program& program);
     void random(OpCode& opCode);
@@ -52,14 +65,7 @@ public:
     OpCode getRandomOpCode();
     CodeMutation getRandomCodeMutation();
 
-    Program generateProgram(int programSize = 100);
-
-
     void mutate(Code& code, CodeMutation mutation);
-    void mutate(Program& program);
-
-	std::default_random_engine randEngine;
-	std::uniform_int_distribution<int> randomOperand;
-	std::uniform_int_distribution<int> randomRegister;
-	std::uniform_int_distribution<int> randomMutate;
+    
+    std::default_random_engine randEngine;
 };
